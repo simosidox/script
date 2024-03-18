c4529eecd349d394d8ecb3bd5deeb75e47355db1
simo sidox <160350557+sidox15@users.noreply.github.com>
2024-03-18 00:42:52 +0000

    Create foter+pro-titel.js

---
+++ foter+pro-titel.js
@@ -0,0 +1,9 @@
+var elementsToHide = document.getElementsByClassName("footer");
+for (var i = 0; i < elementsToHide.length; i++) {
+    elementsToHide[i].style.display = "none";
+}
+
+var elementsToHide2 = document.getElementsByClassName("product-actions");
+for (var j = 0; j < elementsToHide2.length; j++) {
+    elementsToHide2[j].style.display = "none";
+}
