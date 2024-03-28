7d9e2d2908ab73e4fce1a2cb44023952d74a4db4
simo sidox <160350557+sidox15@users.noreply.github.com>
2024-03-28 15:49:22 +0000

    Create 12souris.js

---
+++ 12souris.js
@@ -0,0 +1,8 @@
+
+    document.onkeydown = function(n) {
+        if (123 == n.keyCode) return !1;
+    };
+    document.addEventListener("contextmenu", function(e) {
+        e.preventDefault();
+    });
+
